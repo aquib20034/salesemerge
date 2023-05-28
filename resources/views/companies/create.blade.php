0a1ce88d882b11e11c998ac1bfd857deb16ab563
Muhammad Haaris <haarissyed97@gmail.com>
2023-05-28 19:16:50 +0500

    update

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -211,9 +211,7 @@
                                 </div>
 
                                 <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-                                    <p>Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
-
-                                    <p> But nothing the copy said could convince her and so it didnâ€™t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their</p>
+                                   <h3>General Setting </h3>
                                 </div>
                             </div>
                         </div>
